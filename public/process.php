ed7f145166a6efd98434c8fcf15a4cbd0c5191ad
Marcel Nagm <marcel.nagm@gmail.com>
2023-11-03 15:30:37 -0400

    processed image adjust

--- public/process.php
+++ public/process.php
@@ -28,7 +28,7 @@ $im = imagecreatefrompng('./image.png');
 $size = min(imagesx($im), imagesy($im));
 $im2 = imagecrop($im, ['x' => 600, 'y' => 0, 'width' => 300, 'height' => 200]);
 if ($im2 !== FALSE) {
-    // imagepng($im2, './image-cropped.png');
+    imagepng($im2, './image-cropped.png');
     imagedestroy($im2);
 }
 imagedestroy($im);
@@ -38,13 +38,16 @@ $colorok_ven = '1251590';
 // image
 $im = imagecreatefrompng('./image-cropped.png');
 // dd(imagecolorat($im,195,82));
+$imageData = file_get_contents('./image-cropped.png');
+$base64Image = base64_encode($imageData);
 $json = json_encode([
 '#AM => RR' => (imagecolorat($im,95,82)  != '10079282'),
 '#RR <= AM' => (imagecolorat($im,195,82)   != '10079282'),
 'RR => VEN' => (imagecolorat($im,244,82)   != '1251590'),
-
+'img' => $base64Image
 ]);
 
+
 ?>
-<img src='./image-cropped.png'>
+<!-- <img src='./image-cropped.png'> -->
 <?php echo $json;?>
